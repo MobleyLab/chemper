e82242a7cfa08bd7e4c5f733165dbeaae617ae91
bannanc <bannanc@uci.edu>
2018-02-28 20:12:36 -0800

    start drafting molecule toolkit adapters and their openeye version

---
+++ chemical_perception/mol_toolkits/adapters.py
@@ -0,0 +1,121 @@
+"""
+adapters
+
+This script contains adapters or the structure for atoms, molecules and
+substructure searches.
+Our chemical perception code is designed to be independent of the users
+cheminformatics packages. For each cheminformatics package we support we
+will provide classes following the structure in these adapters.
+"""
+
+from abc import ABC, abstractmethod
+
+# =======================================
+# Molecule Class
+# =======================================
+
+class MolAdapter(ABC):
+    @abstractmethod
+    def __init__(self, mol):
+        return
+
+    @abstractmethod
+    def get_atoms(self):
+        return
+
+    @abstractmethod
+    def get_atom_by_index(self, idx):
+        return
+
+    def get_bonds(self):
+        return
+
+    @abstractmethod
+    def substructure_search(self, search_pattern):
+        return
+
+# =======================================
+# Atom Class
+# =======================================
+
+def AtomAdapter(ABC):
+    @abstractmethod
+    def __init__(self, atom):
+        return
+
+    @abstractmethod
+    def atomic_number(self):
+        return
+
+    @abstractmethod
+    def degree(self):
+        return
+
+    @abstractmethod
+    def connectivity(self):
+        return
+
+    @abstractmethod
+    def valence(self):
+        return
+
+    @abstractmethod
+    def formal_charge(self):
+        return
+
+    @abstractmethod
+    def hydrogen_count(self):
+        return
+
+    @abstractmethod
+    def min_ring_size(self):
+        return
+
+    @abstractmethod
+    def ring_connectivity(self):
+        return
+
+    @abstractmethod
+    def is_aromatic(self):
+        return
+
+    @abstractmethod
+    def get_index(self):
+        return
+
+    @abstractmethod
+    def is_connected_to(self, atom2):
+        return
+
+    @abstractmethod
+    def get_neighbors(self):
+        return
+
+    @abstractmethod
+    def get_bonds(self):
+        return
+
+
+# =======================================
+# Bond Class
+# =======================================
+
+class BondAdapter(ABC):
+    @abstractmethod
+    def __init__(self, bond):
+
+    @abstractmethod
+    def get_order(self):
+        return
+
+    @abstractmethod
+    def get_atoms(self):
+        return
+
+    @abstractmethod
+    def is_ring(self):
+        return
+
+    @abstractmethod
+    def is_aromatic(self):
+        return
---
+++ chemical_perception/mol_toolkits/cp_openeye.py
@@ -0,0 +1,86 @@
+"""
+Cheminformatics tools using OpenEye Toolkits
+
+The classes provided here follow the structure in adapters.
+This is a wrapper allowing our actual package to use openeye toolkits
+"""
+
+from adapters import MolAdapter, AtomAdapter
+from openeye import oechem
+
+
+# =======================================
+# Molecule Class
+# =======================================
+
+class MolOE(MolAdapter):
+    def __init__(self, mol):
+        self.mol = mol
+        return
+
+    def get_atoms(self):
+        return [AtomOE(a) for a in self.mol.GetAtoms()]
+
+    def get_atom_by_index(self, idx):
+        return AtomOE(mol.GetAtom(oechem.OEHasAtomIdx(idx))
+
+    def smirks_search(self, smirks):
+        matches = list()
+
+        ss = oechem.OESubSearch()
+        if not ss.Init(smirks):
+            #TODO: write custom exceptions?
+            raise Exception("Error parsing SMIRKS %s" % smirks)
+
+        for match in ss.Match(self.mol, False):
+            d = dict()
+            for ma in match.GetAtoms():
+                atom_idx = ma.pattern.GetMapIdx()
+                if atom_idx !=0:
+                    d[atom_idx] = AtomOE(ma.target)
+            matches.append(d)
+
+        return matches
+
+
+# =======================================
+# Atom Class
+# =======================================
+
+
+def AtomOE(AtomAdapter):
+    def __init__(self, atom):
+        self.atom = atom
+
+    def atomic_number(self):
+        return self.atom.GetAtomicNum()
+
+    def degree(self):
+        return self.atom.GetDegree()
+
+    def connectivity(self):
+        return len([b for b in self.atom.GetBonds()])
+
+    def valence(self):
+        return self.atom.GetValence()
+
+    def formal_charge(self):
+        return self.atom.GetFormalCharge()
+
+    def hydrogen_count(self):
+        return self.atom.GetTotalHCount()
+
+    def ring_connectivity(self):
+        return len([b for b in self.atom.GetBonds(oechem.OEBondIsInRing())])
+
+    def min_ring_size(self):
+        return oechem.OEAtomGetSmallestRingSize(self.atom)
+
+    def is_aromatic(self):
+        return a.IsAromatic()
+
+    def get_index(self):
+        return self.atom.GetIdx()
+
+    def is_connected_to(self, atom2):
+        return self.atom.IsConnected(atom2.atom)
