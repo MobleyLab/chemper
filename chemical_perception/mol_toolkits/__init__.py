fed6b345b7ca47c6e1a85b2fb589f3e3756e9049
bannanc <bannanc@uci.edu>
2018-03-29 16:19:11 -0700

    fixing init file

--- chemical_perception/mol_toolkits/__init__.py
+++ chemical_perception/mol_toolkits/__init__.py
@@ -1,11 +1,9 @@
 from . import adapters
 try:
-    import from . import cp_openeye as mol_toolkit
+    from . import cp_openeye as mol_toolkit
 except ImportError:
-    pass
-
-try:
-    from . import cp_rdk as mol_toolkit
-except Exception as e:
-    print(e)
-    print("Warning: No cheminformatics package (openeye, rdkit) was found")
+    try:
+        from . import cp_rdk as mol_toolkit
+    except Exception as e:
+        print(e)
+        print("Warning: No cheminformatics package (openeye, rdkit) was found")
