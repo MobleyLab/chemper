7bf81963562e547ddd8b6a83f22b963144e35fee
bannanc <bannanc@uci.edu>
2018-06-20 18:21:02 -0700

    expand tests for more thorough coverage and corner cases

--- tests/test_graphs_output.py
+++ tests/test_graphs_output.py
@@ -2,33 +2,36 @@
 This test is created to check how a single molecule SMIRKS graph works
 """
 
-from chemper.graphs.fragment_graph import ChemPerGraphFromMol
 from chemper.graphs.cluster_graph import ClusterGraph
+from chemper.graphs.fragment_graph import ChemPerGraphFromMol, ChemPerGraph
 from chemper.mol_toolkits import mol_toolkit
 import pytest
 
 
 def make_frag_graph(smiles, layers):
+    """
+    Generates a chemper Mol from the provided smiles
+    a ChemPerGraphFromMol with that molecules with the layers specified
+    """
     mol = mol_toolkit.MolFromSmiles(smiles)
     smirks_dict = {1:0, 2:1}
     return ChemPerGraphFromMol(mol, smirks_dict, layers)
 
 def make_cluster_graph(smiles_list, layers=0):
+    """
+    Generates molecules for the smiles in smiles_list and then creates
+    a ClusterGraph with those molecules with the layers specified
+    """
     smirks_dict_list = [[{1:0, 2:1}]]*len(smiles_list)
     mols_list = [mol_toolkit.MolFromSmiles(smiles) for smiles in smiles_list]
     return ClusterGraph(mols_list, smirks_dict_list, layers=layers)
 
 
+# Check for expected output
 insideH = "(-;!@[#1AH0X1x0r0+0])(-;!@[#1AH0X1x0r0+0])(-;!@[#1AH0X1x0r0+0])"
 outsideH = "(-;!@[#1AH0X1x0r0+0])(-;!@[#1AH0X1x0r0+0])-;!@[#1AH0X1x0r0+0]"
+outsideH_frag = "(-!@[#1AH0X1x0r0+0])(-!@[#1AH0X1x0r0+0])-!@[#1AH0X1x0r0+0]"
 graph_data = [
-    (make_frag_graph('C', 0), '[#6AH4X4x0r0+0:1]-!@[#1AH0X1x0r0+0:2]'), # no layers
-    (make_frag_graph('C#C', 1), # one layer
-     '[#6AH1X2x0r0+0:1](-!@[#1AH0X1x0r0+0])#!@[#6AH1X2x0r0+0:2]-!@[#1AH0X1x0r0+0]'),
-    (make_frag_graph('CO', 'all'), # infinite layers
-     '[#6AH3X4x0r0+0:1](-!@[#1AH0X1x0r0+0])(-!@[#1AH0X1x0r0+0])(-!@[#1AH0X1x0r0+0])'\
-     '-!@[#8AH1X2x0r0+0:2]-!@[#1AH0X1x0r0+0]'
-     ),
     (make_cluster_graph(['CC']), "[#6AH3X4x0r0+0:1]-;!@[#6AH3X4x0r0+0:2]"),
     (make_cluster_graph(['CC', 'C=C']),
      "[#6AH2X3x0r0+0,#6AH3X4x0r0+0:1]-,=;!@[#6AH2X3x0r0+0,#6AH3X4x0r0+0:2]"),
@@ -46,6 +49,15 @@ graph_data = [
      ),
     (make_cluster_graph(['C#CC'], 3), # one layer
      '[#6AH1X2x0r0+0:1](-;!@[#1AH0X1x0r0+0])#;!@[#6AH0X2x0r0+0:2]-;!@[#6AH3X4x0r0+0]%s' % outsideH),
+    (make_frag_graph('C', 0), '[#6AH4X4x0r0+0:1]-!@[#1AH0X1x0r0+0:2]'), # no layers
+    (make_frag_graph('C#C', 1), # one layer
+     '[#6AH1X2x0r0+0:1](-!@[#1AH0X1x0r0+0])#!@[#6AH1X2x0r0+0:2]-!@[#1AH0X1x0r0+0]'),
+    (make_frag_graph('C#CC', 3), # three layers
+     '[#6AH1X2x0r0+0:1](-!@[#1AH0X1x0r0+0])#!@[#6AH0X2x0r0+0:2]-!@[#6AH3X4x0r0+0]%s' % outsideH_frag),
+    (make_frag_graph('CO', 'all'), # infinite layers
+     '[#6AH3X4x0r0+0:1](-!@[#1AH0X1x0r0+0])(-!@[#1AH0X1x0r0+0])(-!@[#1AH0X1x0r0+0])' \
+     '-!@[#8AH1X2x0r0+0:2]-!@[#1AH0X1x0r0+0]'
+     ),
 ]
 
 
@@ -81,14 +93,3 @@ def test_layered_clusters(graph, expected):
     print(smirks)
     assert smirks in expected
 
-
-# make sure corner cases can at least be built without failing
-smiles = [['C', 'c1ccccc1'], ['C1CC1F', 'OC(=O)CC'], ['c1ccccc1', 'Oc1ccccc1']]
-layers_options = [1,2,3,4,5,'all']
-combos = [(s,l) for s in smiles for l in layers_options]
-
-@pytest.mark.parametrize('smiles_list,layers', combos)
-def test_no_failing(smiles_list, layers):
-    smirks_dict_list = [[{1: 0, 2: 1}, {1:1, 2:2}]] * len(smiles_list)
-    mols_list = [mol_toolkit.MolFromSmiles(s) for s in smiles_list]
-    ClusterGraph(mols_list, smirks_dict_list, layers=layers)
