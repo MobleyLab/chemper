56c8f068a23c4a0e8f799b0405e964d07e9eb20a
bannanc <bannanc@uci.edu>
2019-04-17 22:42:32 -0700

    remove .chemper from __init__

--- chemper/__init__.py
+++ chemper/__init__.py
@@ -3,9 +3,6 @@ chemper
 Automated Chemical Perception for SMIRNOFF formatted force fields
 """
 
-# Add imports here
-from .chemper import *
-
 # Handle versioneer
 from ._version import get_versions
 versions = get_versions()
