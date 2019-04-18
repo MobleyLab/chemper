2dd8081750bb9835d597f4ce767e1659d905a3b4
bannanc <bannanc@uci.edu>
2019-04-17 22:23:54 -0700

    update __init__ files

--- chemper/__init__.py
+++ chemper/__init__.py
@@ -1,4 +1,14 @@
-from . import mol_toolkits
-from . import graphs
-from . import chemper_utils
-from . import smirksify
+"""
+chemper
+Automated Chemical Perception for SMIRNOFF formatted force fields
+"""
+
+# Add imports here
+from .chemper import *
+
+# Handle versioneer
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
--- chemper/graphs/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import fragment_graph
-from . import environment
-from . import cluster_graph
