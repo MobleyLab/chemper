331689f21fdd32f15d3aa738ba267db2013197d6
bannanc <bannanc@uci.edu>
2019-07-03 17:54:58 -0700

    fix test_examples so it actually finds *.py files and runs them

--- chemper/tests/test_examples.py
+++ chemper/tests/test_examples.py
@@ -25,6 +25,15 @@ import pytest
 ROOT_DIR_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..')
 
 
+def run_script_file(file_path):
+    """Run through the shell a python script."""
+    cmd = ['python', file_path]
+    try:
+        subprocess.check_call(cmd)
+    except subprocess.CalledProcessError:
+        raise Exception('Example {file_path} failed'.format(file_path=file_path))
+
+
 def run_script_str(script_str):
     """
     Execute a Python string through the shell in a temporary directory.
@@ -48,13 +57,17 @@ def run_script_str(script_str):
             script_str = textwrap.indent(script_str, '    ')
             raise Exception('The following script failed:\n'+script_str)
 
-def find_python_examples(dir_name='examples'):
+
+def find_python_examples(dir_name='examples', sub_dirs=0):
     """Find all examples in the specified directory.
+    That is look for *.py files to be tested.
 
     Parameters
     ----------
     dir_name : str
               directory path from root directory with python files
+    sub_dirs : idx
+        How many sub-directories should we look for python files in?
 
     Returns
     -------
@@ -64,10 +77,20 @@ def find_python_examples(dir_name='examples'):
     examples_dir_path = os.path.join(ROOT_DIR_PATH, dir_name)
 
     example_file_paths = []
-    for example_file_path in glob.glob(os.path.join(examples_dir_path, '*', '*.py')):
+    # check provided directory
+    test_pys = glob.glob(os.path.join(examples_dir_path, '*.py'))
+
+    # look in subdirectories
+    current_dirs = examples_dir_path
+    for idx in range(sub_dirs):
+        current_dirs = os.path.join(current_dirs, '*')
+        test_pys += glob.glob(os.path.join(current_dirs, '*.py'))
+
+    for example_file_path in test_pys:
         example_file_path = os.path.relpath(example_file_path)
         example_file_paths.append(example_file_path)
     return example_file_paths
+
 
 def scrape_md_file(md_path):
     """
@@ -110,12 +133,14 @@ for dir, sub_dirs, files in os.walk(ROOT_DIR_PATH):
             py_examples += pys
             url_examples += us
 
+
 @pytest.mark.parametrize('py_str', py_examples)
 def test_py_examples(py_str):
     """
     Test python example
     """
     run_script_str(py_str)
+
 
 @pytest.mark.parametrize('link', url_examples)
 def test_urls(link):
@@ -131,9 +156,19 @@ def test_urls(link):
     request = Request(link, headers=headers)
     urlopen(request)
 
-@pytest.mark.parametrize('py_script', find_python_examples())
+
+py_scripts = find_python_examples(sub_dirs=2)
+py_scripts += find_python_examples('docs', 2)
+
+
+@pytest.mark.parametrize('py_script', py_scripts)
 def test_script_examples(py_script):
     """
     Test that *.py files from examples run without errors
     """
-    run_script_file(py_script)
+    f = open(py_script, 'r')
+    text = f.read()
+    f.close()
+
+    run_script_str(text)
+    #run_script_file(py_script)
